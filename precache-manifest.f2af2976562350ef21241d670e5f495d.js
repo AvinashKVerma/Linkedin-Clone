02bc2dbc116ba13b38523b5c2cbd690471ecddcf
Avinash Kumar Verma <avinashverma078@gmail.com>
2023-07-24 17:29:41 +0530

    Updates

---
+++ precache-manifest.f2af2976562350ef21241d670e5f495d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "690c287b30b70d2dfcdb",
+    "url": "/Linkedin-Clone/static/css/main.593d5b23.chunk.css"
+  },
+  {
+    "revision": "690c287b30b70d2dfcdb",
+    "url": "/Linkedin-Clone/static/js/main.a01406cb.chunk.js"
+  },
+  {
+    "revision": "f8f4fd71036425fee83a",
+    "url": "/Linkedin-Clone/static/js/runtime~main.28ce3031.js"
+  },
+  {
+    "revision": "05593177f5d7a0c060dc",
+    "url": "/Linkedin-Clone/static/js/2.17715bda.chunk.js"
+  },
+  {
+    "revision": "1e81c8e11f04ce4e8396",
+    "url": "/Linkedin-Clone/static/js/3.c0194158.chunk.js"
+  },
+  {
+    "revision": "eb449b73b3142d65b1d953ca09f923c4",
+    "url": "/Linkedin-Clone/static/media/dxf91zhqd2z6b0bwg85ktm5s4.eb449b73.svg"
+  },
+  {
+    "revision": "4de8d316502d25ad6611f916229fb0f4",
+    "url": "/Linkedin-Clone/index.html"
+  }
+];
